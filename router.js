cb62da8cf2ad3b329072c92422d0a049c2f37751
alex <1464911422@qq.com>
2016-12-18 11:10:18 +0800

    change the structure of the project to adapte the angular router
    Signed-off-by: alex <1464911422@qq.com>

--- router.js
+++ router.js
@@ -2,14 +2,23 @@
  * Created by alex on 2016/12/4.
  */
 myApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("index");
+    $urlRouterProvider.otherwise("bootstrap");
     $stateProvider
         .state("index", {
-            url: "/index",
+            url: "/",
             templateUrl: "views/nav.html"
         })
-        .state("index.home", {
-            url: "/home",
-            templateUrl: "views/home.html"
+        .state("bootstrap",{
+            url:"/bootStrap",
+            templateUrl:"views/BootstrapPages/Gy-bootstrap.html"
+        })
+        .state("angular",{
+            url:"/angular",
+            templateUrl:"views/AngularPages/Gy-angular.html"
+        })
+        .state('springboot',{
+            url:"/springboot",
+            templateUrl:"views/SpringBootPages/Gy-springboot.html"
         });
+
 });
