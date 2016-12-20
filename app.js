4c00d1f9c1fcbafa679c59c11e324cfaaa4ebf06
alex <1464911422@qq.com>
2016-12-20 20:09:59 +0800

    add quit system function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var myApp=angular.module("Daniella",["ui.router"]);
 
 myApp.run(function($rootScope){
-   $rootScope.isLogin=false;
+    $rootScope.isLogin=false;
+    $rootScope.signOut=function(){
+        console.log("bye-bye");
+        $rootScope.isLogin=false;
+        document.cookie="isLogin="+false;
+    }
 });
